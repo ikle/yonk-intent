636a1f2b04eeecafc4afb2b67f4c4581208ac8a0
Alexei A. Smekalkine <ikle@ikle.ru>
2023-10-02 11:30:38 +0300

    Add rib core API implementation

---
+++ include/yonk/rib.h
@@ -0,0 +1,68 @@
+/*
+ * Yonk Rib: Low-Level State and Configuration Node
+ *
+ * Copyright (c) 2022-2023 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef YONK_RIB_H
+#define YONK_RIB_H  1
+
+#include <string.h>
+
+#include <capsa/tree.h>
+
+enum yonk_kind {
+	YONK_GROUP = 0,
+	YONK_NODE,
+	YONK_TAG,
+	YONK_ATTR,
+	YONK_VALUE,
+};
+
+TREE_DECLARE_TYPE (yonk_rib)
+
+struct yonk_rib {
+	struct yonk_rib *parent;
+	struct avl node;
+	int kind, mode;
+	char *name;
+	struct yonk_rib_tree childs;
+};
+
+struct yonk_rib *yonk_rib_alloc (const char *name);
+void yonk_rib_free (struct yonk_rib *o);
+
+static inline int yonk_rib_order (const char *key, const struct yonk_rib *o)
+{
+	return strcmp (key, o->name);
+}
+
+TREE_DECLARE_CORE (yonk_rib)
+
+static inline
+struct yonk_rib *yonk_rib_find (struct yonk_rib *o, const char *name)
+{
+	return yonk_rib_tree_lookup (&o->childs, name);
+}
+
+static inline
+struct yonk_rib *yonk_rib_add (struct yonk_rib *o, int kind, const char *name)
+{
+	struct yonk_rib *n;
+
+	if ((n = yonk_rib_tree_insert (&o->childs, name)) == NULL)
+		return n;
+
+	n->parent = o;
+	n->kind = kind;
+	return n;
+}
+
+static inline void yonk_rib_del (struct yonk_rib *o, const char *name)
+{
+	yonk_rib_free (yonk_rib_tree_remove (&o->childs, name));
+}
+
+#endif  /* YONK_RIB_H */
---
+++ yonk-rib-core.c
@@ -0,0 +1,40 @@
+/*
+ * Yonk Rib Core
+ *
+ * Copyright (c) 2022-2023 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stdlib.h>
+
+#include <yonk/rib.h>
+
+struct yonk_rib *yonk_rib_alloc (const char *name)
+{
+	struct yonk_rib *o;
+
+	if ((o = malloc (sizeof (*o))) == NULL)
+		return o;
+
+	if ((o->name = strdup (name)) == NULL) {
+		free (o);
+		return NULL;
+	}
+
+	o->parent = NULL;
+	o->kind = YONK_GROUP;
+	o->mode = 0;
+	yonk_rib_tree_init (&o->childs);
+	return o;
+}
+
+void yonk_rib_free (struct yonk_rib *o)
+{
+	if (o == NULL)
+		return;
+
+	yonk_rib_tree_fini (&o->childs);
+	free (o->name);
+	free (o);
+}
